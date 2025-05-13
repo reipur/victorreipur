93423cbcf228f46545508538fbc79df4574fb164
Victor Reipur <victor.reipur@gmail.com>
2025-05-13 19:30:34 +0000

    added /cv proxy

--- app-main/next.config.js
+++ app-main/next.config.js
@@ -1,6 +1,18 @@
 // next.config.js
-export default {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['supabase.victorreipur.dk'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/cv',
+        destination:
+          'https://supabase.victorreipur.dk/storage/v1/object/public/public-bucket/Victor_Reipur_CV.pdf',
+      },
+    ];
+  },
 };
+
+export default nextConfig;
